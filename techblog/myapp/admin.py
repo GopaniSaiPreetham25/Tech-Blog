a030362b5eb6a23c7183a45fcea6ba6b2a0562b0
shaikhussain91 <shaikhussainbaba9@gmail.com>
2025-01-23 19:58:05 +0530

    new

--- techblog/myapp/admin.py
+++ techblog/myapp/admin.py
@@ -1,11 +1,20 @@
+# from django.contrib import admin
+# from django.apps import apps
+
+# # Lazy model import to avoid circular import
+# techblog = apps.get_model('myapp', 'techblog')
+
+# @admin.register(techblog)
+# class TechblogAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'discription', 'photo')  # Display these fields in admin
+#     search_fields = ('title', 'discription')          # Add search functionality
+#     list_filter = ('title',)                          # Filter options by title
+
+
 from django.contrib import admin
-from django.apps import apps
+from .models import techblog
 
-# Lazy model import to avoid circular import
-techblog = apps.get_model('myapp', 'techblog')
-
-@admin.register(techblog)
 class TechblogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'discription', 'photo')  # Display these fields in admin
-    search_fields = ('title', 'discription')          # Add search functionality
-    list_filter = ('title',)                          # Filter options by title
+    list_display = ('title', 'discription', 'photo')
+
+admin.site.register(techblog, TechblogAdmin)
