d4b4c233dfe008018841a47dd6e6c567be73f7b6
shaikhussain91 <shaikhussainbaba9@gmail.com>
2025-01-23 19:42:20 +0530

    new

--- techblog/myapp/models.py
+++ techblog/myapp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class techblog(models.Model):
-    tittle=models.CharField(max_length=100)
+    title=models.CharField(max_length=100)
     discription=models.TextField()
     photo=models.ImageField(upload_to='pics')
