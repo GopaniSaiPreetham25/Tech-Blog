5dfa0e5ec3a4816f04605c93d143c0e9324f4577
GopaniSaiPreetham25 <saipreetham9666@gmail.com>
2025-01-24 14:24:10 +0530

    new

--- techblog/myapp/models.py
+++ techblog/myapp/models.py
@@ -2,12 +2,7 @@ from django.db import models
 
 
 class techblog(models.Model):
-
-    tittle=models.CharField(max_length=100)
-    discription=models.TextField() 
-    photo=models.ImageField(upload_to='pics')
-
     title=models.CharField(max_length=100)
     discription=models.TextField() 
-    photo=models.ImageField(upload_to='pics') 
+    photo=models.ImageField(upload_to='pics')
 
