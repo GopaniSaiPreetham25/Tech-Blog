224ab5b8ae097b652866a6af66a39e270ea064ce
shaikhussain91 <shaikhussainbaba9@gmail.com>
2025-01-24 12:40:22 +0530

    new

--- techblog/myapp/views.py
+++ techblog/myapp/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from .models import techblog
 from .forms import techblogForm
 
-def techblog_list(request):
-    techblogs = techblog.objects.all()
-    return render(request, 'techblog/techblog_list.html', {'techblogs': techblogs})
+
+def techblog(request):
+    return render(request, 'techblog.html', {'techblogs': techblogs})
