44b77323ec331e950de2686700dc274abff85c83
shaikhussain91 <shaikhussainbaba9@gmail.com>
2025-01-25 19:14:29 +0530

    new

--- techblog/myapp/forms.py
+++ techblog/myapp/forms.py
@@ -3,14 +3,10 @@ from .models import techblog
 
 from django.contrib.auth.models import User
 
-<<<<<<< HEAD
 # forms.py
  # Replace with your actual model
 
 class CreateAccountForm(forms.ModelForm):
-=======
-class createaccountForm(forms.ModelForm):
->>>>>>> f63e2532fa690bf2cb27ff3d6d3753b08b532412
     class Meta:
         model = User
         fields=['username','first_name','last_name','password']
