d2e79a97921bd14258e370560e050524d3e1dee2
GopaniSaiPreetham25 <saipreetham9666@gmail.com>
2025-01-25 13:12:33 +0530

    new

--- techblog/myapp/urls.py
+++ techblog/myapp/urls.py
@@ -6,5 +6,7 @@ app_name='myapp'
 urlpatterns=[
     path('insert/',insert,name='insert'),
     path('viewdata/',viewdata,name='viewdata'),
+    path('update/<int:id>',update,name='update'),
+    path('delete/<int:id>',delete,name='delete'),
     path('home/',home,name='home'),
 ]
