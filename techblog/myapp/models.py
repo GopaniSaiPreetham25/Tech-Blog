316ae659e7fb67f9ea4756b959a65df5df43c000
NagaTanmayi <nagatanmayi31@gmail.com>
2025-01-28 14:20:32 +0530

    new

--- techblog/myapp/models.py
+++ techblog/myapp/models.py
@@ -6,10 +6,10 @@ class techblog(models.Model):
     title=models.CharField(max_length=100)
     discription=models.TextField() 
     photo=models.ImageField(upload_to='pics') 
-class techblog(models.Model):
-    title=models.CharField(max_length=100)
-    discription=models.TextField() 
-    photo=models.ImageField(upload_to='pics')
+# class techblog(models.Model):
+#     title=models.CharField(max_length=100)
+#     discription=models.TextField() 
+#     photo=models.ImageField(upload_to='pics')
     
 
 
