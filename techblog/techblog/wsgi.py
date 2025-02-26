66f1347e171bdf364f2a0b3603bb99030516d1d1
GopaniSaiPreetham25 <saipreetham9666@gmail.com>
2025-02-26 12:25:34 +0530

    new

--- techblog/techblog/wsgi.py
+++ techblog/techblog/wsgi.py
@@ -7,15 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techblog.settings')
+
+# application = get_wsgi_application()
+
+# app=application
+
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techblog.settings')
 
-application = get_wsgi_application()
-
-app=application
-
+app = get_wsgi_application()
 
 
